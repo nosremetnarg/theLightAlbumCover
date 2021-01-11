ff5200faf4a297539e6cb28c69dcb9f649b2bcdb
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-11 15:16:50 -0600

    had to add inline styles in html to get it to work

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const picture1 = document.getElementById("picture1");
 const picture2 = document.getElementById("picture2");
 const body = document.querySelector("body");
-const picture3 = document.querySelector(".image1");
+const picture3 = document.querySelector("#picture1");
 
 
 function changeDisplay() {
     console.log("clicked");
-    console.log(picture3.style);
+    console.log(picture3.style.display);
     if (picture3.style.display === "block" ) {
         console.log('p1 display block');
-        picture1.style.display = "none";
+        picture3.style.display = "none";
         picture2.style.display = "block";
-    } else if (picture1.style.display === "none") {
-        picture1.style.display = "block";
+    } else if (picture3.style.display === "none") {
+        picture3.style.display = "block";
         picture2.style.display = "none";
     }
 
